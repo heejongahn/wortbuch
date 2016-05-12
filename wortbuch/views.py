328fed587bb27efa87ccd2aceb8ae617162572d2
Heejong Ahn <heejongahn@gmail.com>
2016-05-12 13:50:20 +0200

    Break a function into smaller size

--- wortbuch/views.py
+++ wortbuch/views.py
@@ -11,43 +11,46 @@ def translate(origin, word):
     url = 'http://www.linguee.com/german-english/search?source={}&query={}'.\
             format(origin, word)
 
-    words = parse_linguee(url)
+    lemmas = get_lemmas(url)
+
+    words = []
+    for lemma in lemmas:
+        word = parse_lemma(lemma)
+        words.append(word)
 
     return str(words)
 
 
-def parse_linguee(url):
+def parse_lemma(lemma):
+    word = {'translations': []}
+
+    try:
+        dict_term_elm = lemma.find_class('dictTerm')[0]
+    except:
+        continue
+
+    word_type_elm = lemma.find_class('tag_wordtype')[0]
+    trans_elms = lemma.find_class('tag_trans')
+
+    word['dict_term'] = dict_term_elm.text
+    word_type = word_type_elm.text.split(',\xa0')
+
+    word['word_class'] = word_type[0]
+    if len(word_type) > 1:
+        word['gender'] = word_type[1]
+
+    for trans_elm in trans_elms:
+        trans_elm = list(trans_elm)
+
+        translation = {}
+        translation['meaning'] = trans_elm[0].text_content().split()[-1]
+        translation['tag'] = trans_elm[1].text
+        word['translations'].append(translation)
+
+    return word
+
+def get_lemmas_from_linguee(url):
     tree = html.parse(url)
     exact = tree.getroot().find_class('exact')[0]
 
-    words = []
-
-    for lemma in exact.find_class('lemma'):
-        word = {'translations': []}
-
-        try:
-            dict_term_elm = lemma.find_class('dictTerm')[0]
-        except:
-            continue
-
-        word_type_elm = lemma.find_class('tag_wordtype')[0]
-        trans_elms = lemma.find_class('tag_trans')
-
-        word['dict_term'] = dict_term_elm.text
-        word_type = word_type_elm.text.split(',\xa0')
-
-        word['word_class'] = word_type[0]
-        if len(word_type) > 1:
-            word['gender'] = word_type[1]
-
-        for trans_elm in trans_elms:
-            trans_elm = list(trans_elm)
-
-            translation = {}
-            translation['meaning'] = trans_elm[0].text_content().split()[-1]
-            translation['tag'] = trans_elm[1].text
-            word['translations'].append(translation)
-
-        words.append(word)
-
-    return words
+    return exact.find_class('lemma')

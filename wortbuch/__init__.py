dfdea75023e69191dd8346ea29fa8b6462140107
Heejong Ahn <heejongahn@gmail.com>
2016-06-16 22:29:28 +0200

    Introduce Flask-Bcrpyt

--- wortbuch/__init__.py
+++ wortbuch/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_wtf.csrf import CsrfProtect
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -10,6 +11,7 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 
+bcrypt = Bcrypt(app)
 CsrfProtect(app)
 
 db = SQLAlchemy(app)
